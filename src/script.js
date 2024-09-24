b695b2d2cb1a57ffc386185f1cb89d216c3c9fdb
Akiraaa07 <igorakira2004@gmail.com>
2024-09-24 14:17:53 -0300

    Entregavel da calculadora completo

--- src/script.js
+++ src/script.js
@@ -3,6 +3,37 @@
 
 //ESCREVA SEU CÓDIGO AQUI!!
 
+function calculadora(num1, num2, operacao){
+  let resultado;
+  num1 = parseInt(num1);
+  num2 = parseInt(num2);
+
+  switch(operacao){
+    case "+":
+      resultado = num1 + num2
+      break;
+    case "-":
+      resultado = num1 - num2
+      break;
+    case "*":
+      resultado = num1 * num2
+      break;
+    case "/":
+      resultado = num1 / num2
+      break;
+    case "e": // Potência
+    resultado = 1;
+    for (let i = 0; i < num2; i++) {
+      resultado *= num1; 
+    }
+    break;
+  }
+  
+  if (resultado == undefined || resultado > 1000000)
+    resultado = "Erro!!"
+
+  return resultado
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -26,5 +57,5 @@ function calcular() {
       operador = x
   });
 
-  visor.value = Calculadora(numeros[0], numeros[1], operador)
+  visor.value = calculadora(numeros[0], numeros[1], operador)
 }
